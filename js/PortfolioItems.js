ca0460b1354d4f6223739911b43b65f07f5092b1
Rakesh <rakeshkumar@myadvo.in>
2019-10-24 14:19:49 +0530

    protfolio images

---
+++ js/PortfolioItems.js
@@ -0,0 +1,20 @@
+var PortfolioItems = [
+    { title: 'FAQ Landing Page', image: 'FAQ-landing.png', desc: '' },
+    { title: 'FAQ Landing Page', image: 'First.png', desc: '' },
+    { title: 'Lead Form step 1st', image: 'Form1.png', desc: '' },
+    { title: 'Lead Form step 2nd', image: 'Form2.png', desc: '' },
+    { title: 'Lead Form step 3rd', image: 'Form3.png', desc: '' },
+    { title: 'Question & Answer', image: 'Question-Answer.png', desc: '' },
+    { title: 'FAQ on Quote Page', image: 'Quote-FAQ.jpg', desc: '' },
+    { title: 'Skeleton 1st', image: 'Skeleton-Step1.jpg', desc: '' },
+    { title: 'Skeleton 2nd', image: 'Skeleton-Step2.jpg', desc: '' },
+    { title: 'Skeleton 3rd', image: 'Skeleton-web-Step1.jpg', desc: '' },
+    { title: 'Skeleton 4th', image: 'Skeleton-web-Step2.jpg', desc: '' },
+    { title: 'New Lead Form Step 1', image: 'Step1.png', desc: '' },
+    { title: 'New Lead Form Step 2', image: 'Step2.png', desc: '' },
+    { title: 'New Lead Form Step 3', image: 'Step3.png', desc: '' },
+    { title: 'Landing Page', image: 'large_1.png', desc: '' },
+    { title: 'Landing Page', image: 'large_2.svg', desc: '' },
+    { title: '404 Page', image: 'large_3.jpg', desc: '' },
+    { title: 'Retargeting Modal', image: 'large_4.jpg', desc: '' }
+];
